606fa6d50d2322ad5ee1bfd65b250131e7a70351
Evgeny Cherkashin <eugeneai@irnok.net>
2018-09-25 08:16:37 +0800

    Added list of Debian packages needed to compile chaos_calmer

--- wndr3800/build-wndr3800.sh
+++ wndr3800/build-wndr3800.sh
@@ -7,4 +7,4 @@ cat list-installed.txt | cut -f1 -d" " | sort | tr '\n' ' ' > list.txt
 FILES=`cat list.txt`
 FILES="odhcpd $FILES"
 make image PROFILE="WNDR3700" PACKAGES="$FILES"
-make manifest PROFILE="WNDR3700" PACKAGES="$FILES" | tee MANIFEST.txt
+# make manifest PROFILE="WNDR3700" PACKAGES="$FILES" | tee MANIFEST.txt
